9bcc8fc7b111984d479f19c0ecad2ee9d42c120f
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-03-09 17:57:53 +0900

    refactor

--- hello.js
+++ hello.js
@@ -6,10 +6,12 @@ const schema = buildSchema(`
   }
 `);
 
+const query = '{ hello }';
+
 const root = {
   hello: () => 'Hello world!',
 };
 
-graphql(schema, '{ hello }', root).then(response => {
+graphql(schema, query, root).then(response => {
   console.log({ response });
 });
